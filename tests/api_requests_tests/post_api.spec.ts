4c2c401fa21391c45846e134c048a3928ca6e22e
santcial <santiago.ci9619@gmail.com>
2026-02-09 10:13:49 -0500

    static post api

--- tests/api_requests_tests/post_api.spec.ts
+++ tests/api_requests_tests/post_api.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import post_api from '../../test-data/api-requests/post_api_example.json';
+import post_api from '../../test-data/api-requests/static_post_api_example.json';
 
 test.use({
     baseURL: "https://jsonplaceholder.typicode.com"
@@ -14,7 +14,9 @@ test.describe('POST API Tests', () => {
         console.log(responseBody);
         expect(response.status()).toBe(201);
         expect(response.statusText()).toBe('Created');
-
-
+        expect(responseBody).toHaveProperty('id',101);//  I tried with 1 but JSONPlaceholder always returns id: 101 for POST requests (fake persistence)
+        expect(responseBody).toMatchObject(post_api);//same as above, i had to change the id in the test data to 101 to make this assertion work, otherwise it would fail because of the id mismatch
+        expect(responseBody).toHaveProperty('title', post_api.title);
+        expect(typeof responseBody.id).toBe('number');
     });
 });
