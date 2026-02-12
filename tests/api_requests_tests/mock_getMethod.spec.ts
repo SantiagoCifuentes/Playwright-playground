b5dedbb09061ce76c93e37d64004c1aa32a25a23
santcial <santiago.ci9619@gmail.com>
2026-02-12 18:10:44 -0500

    a

---
+++ tests/api_requests_tests/mock_getMethod.spec.ts
@@ -0,0 +1,15 @@
+
+import { test, expect } from '@playwright/test';
+
+test('mock  get products', async ({ context }) => {
+
+    await context.route('**/products', async route => {
+        
+       
+
+        
+
+});
+
+
+});
