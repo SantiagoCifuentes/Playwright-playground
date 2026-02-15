c2618e010cb70b523ee835763c8363560c068e1d
Santiago <santiago.cifuentes@sofka.com.co>
2026-02-15 15:54:00 -0500

    mocking

--- tests/api_requests_tests/mock_getMethod.spec.ts
+++ tests/api_requests_tests/mock_getMethod.spec.ts
@@ -1,15 +1,32 @@
 
 import { test, expect } from '@playwright/test';
 
-test('mock  get products', async ({ context }) => {
+test('mock  get products', async ({ context, request }) => {
 
-    await context.route('**/products', async route => {
-        
-       
+    await context.route('**/products/1', async route => {
+        await route.fulfill({
+            status: 200,
+            contentType: 'application/json',
+            body: JSON.stringify({
+                id: 1,
+                title: 'iPhone 9',
+                description: 'An apple mobile which is nothing like apple',
+                category: 'smartphones',
+                price: 549,
+                discountPercentage: 12.96
+            })
+            
 
-        
+        });
+        const response =  await request.get('https://dummyjson.com/products/1');
+        const body = await response.json();
 
-});
+        expect(response.status()).toBe(200);
+        expect(body).toHaveProperty('title', 'iPhone 9');
+        expect(body.title).toBe('iPhone 9');
+        expect(route.request().method()).toBe('GET'); // Assert that the request method is GET
+        expect(route.request().url()).toContain('/products/1'); // Assert that the request URL contains the expected endpoint
 
 
+    })
 });
--- tests/api_requests_tests/mocking_withToken.spec.ts
+++ tests/api_requests_tests/mocking_withToken.spec.ts
@@ -1,7 +1,7 @@
 
 import { test, expect } from '@playwright/test';
 
-test('mock  with invalid token', async ({ request, context }) => {
+test('mock with invalid token', async ({ request, context }) => {
 
     await context.route('**/auth/me', async route => {
         const authHeader = route.request().headers()['authorization'];
@@ -19,27 +19,36 @@ test('mock  with invalid token', async ({ request, context }) => {
         }
     });
 
+    const response = await request.get('https://dummyjson.com/auth/me', {
+        headers: {
+            Authorization: 'Bearer invalid-token' // Simulate an invalid token in the request
+        }
+    });
+
+    expect(response.status()).toBe(401);
 });
 
-test('mock with valid token', async ({  context }) => {
+test('mock with valid token', async ({ request, context }) => {
 
-    await context.route('**/auth/me', async route => {
+    await context.route('**/auth/me', async route => { // it doesnt have any  expect because we are just mocking the response and not making any assertions in this test
         await route.fulfill({
             status: 200,
             contentType: 'application/json',
             body: JSON.stringify({
                 id: 15,
                 username: 'emilys',
-                email: 'emily.johnson@x.dummyjson.com',
-                firstName: 'Emily',
-                lastName: 'Johnson',
-                gender: 'female',
-                image: 'https://dummyjson.com/icon/emilys/128'
+                
             })
         });
+    });
 
-    })
-    
+    const response = await request.get('https://dummyjson.com/auth/me', {
+        headers: {
+            Authorization: 'Bearer valid-token'
+        }
+    });
+
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    expect(body.username).toBe('emilys');
 });
-
-
