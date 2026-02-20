c8c196e95f737a5ddc4363e2528eebfb24357b71
santcial <santiago.ci9619@gmail.com>
2026-02-20 15:46:21 -0500

    learning how to handle known bugs in CI

--- tests/sauce_demo_tests/validating_unique_ids.spec.ts
+++ tests/sauce_demo_tests/validating_unique_ids.spec.ts
@@ -2,10 +2,11 @@ import { test, expect } from '@playwright/test';
 
 
 test('detecting duplciated ids, throws an error if duplicated ids are found', {tag: '@unique-ids'},async ({ page }) => {
+    test.fail()// this test is expected to fail because the page contains duplicated ids
   await page.goto('https://sauce-demo.myshopify.com/');
 
   const duplicates = await page.evaluate(() => {
-    const ids = [...document.querySelectorAll('[id]')].map(el => el.id);
+    const ids = Array.from(document.querySelectorAll('[id]')).map(el => el.id);
     return ids.filter((id, i) => ids.indexOf(id) !== i);
   });
 
